d5db981150462ed367f696c2b763dad99938248b
Khalid Bukhari <kb99tx@gmail.com>
2023-09-20 00:53:14 +0300

    initial push

--- src/Sorter.java
+++ src/Sorter.java
@@ -2,7 +2,7 @@ public class Sorter {
     public static void main(String[] args) throws Exception {
         int[] array = {2,3,4,1,3,2};
         for(int i=0;i<array.length;i++){
-            System.out.println(array[i]);
+            System.out.print(array[i]);
         }
     }
 }
