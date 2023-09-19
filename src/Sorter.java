aa40f5a10599c645a44ff6be98aaef3a11e72f3f
Khalid Bukhari <kb99tx@gmail.com>
2023-09-20 00:50:15 +0300

    second

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,6 +1,8 @@
 public class Sorter {
     public static void main(String[] args) throws Exception {
         int[] array = {2,3,4,1,3,2};
-        
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
     }
 }
